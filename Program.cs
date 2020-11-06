c7219712910b95fb8196d498d86847470eaae636
diego_neri <diego@diegoneri.com>
2020-11-06 20:44:14 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int numeroPar = 0;
+
+            Console.WriteLine("Sequência de números pares a partir de zero");
+            Console.Write("Informe um número inteiro positivo: ");
+
+            numero = Convert.ToInt32(Console.ReadLine());
+            
+            if (numero <= 0)
+            {
+                Console.WriteLine($"O número {numero} não é positivo");
+            }
+            else
+            {
+                Console.WriteLine($"Números pares entre 0 e {numero}");
+
+                Console.WriteLine("WHILE: ");
+                while(numeroPar <= numero)
+                {
+                    Console.Write($"{numeroPar} ");
+                    numeroPar = numeroPar + 2;
+                }
+
+                numeroPar = 0;
+                Console.WriteLine("\n\nDO..WHILE: ");
+                do
+                {
+                    Console.Write($"{numeroPar} ");
+                    numeroPar = numeroPar + 2;                    
+                }while(numeroPar <= numero);
+
+                Console.WriteLine("\n\nFOR: ");
+                for(numeroPar = 0; numeroPar <= numero; numeroPar +=2)
+                {
+                    Console.Write($"{numeroPar} ");
+                }
+
+            }
+        }
+    }
+}
